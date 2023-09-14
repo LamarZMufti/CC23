efd2e9493691de7729b718bb7053a6528bd92833
LamarZMufti <144357682+LamarZMufti@users.noreply.github.com>
2023-09-14 04:59:53 +0300

    Still Life

--- sketch.js
+++ sketch.js
@@ -1,47 +1,67 @@
 function setup() {
-  angleMode(DEGREES); //for arc lip function
-  rectMode(CENTER); // a command to center the rect with the ellipse 
-  createCanvas(400, 400); // w, h
-  background(0, 0, 0); //black rgb 
-
-  fill(92, 64, 51); //brown hair color 
-  rect(200, 220, 220, 339, 90); //back hair 
-
-  fill(255, 255, 255); //shirt color white 
-  rect(200, 460, 190, 270, 40); //shirt 
-
-  fill(232,190,172); //face and neck color is nude
-  rect(200, 270, 80, 200, 30); //neck - the 5th parameter makes rounds the edges
-  ellipse(200, 190, 160, 220); // face, x centered y shifted upwards by 190, 160 is for w & 220 is for h
-
-  fill(255, 255, 255); //left and right eye color white
-  ellipse(160, 160, 50, 25); //left eye
-  ellipse(238, 160, 50, 25); //right eye
-
-  fill(92, 64, 51); //color of eyes 
-  ellipse(160, 160, 20, 18); //left eye color
-  ellipse(238, 160, 20, 18); //right eye color 
-
-  fill(0); //pupil color is black
-  ellipse(160, 160, 12, 10); //left eye pupil resized 
-  ellipse(238, 160, 12, 10); //right eye pupil resized
+  createCanvas(400, 400);
+  background(135,206,250);
   
-  fill(144, 44, 62); //velvet lip color 
-  arc(200, 236, 60, 50, 0, 180); //180 is angle for smile
-
-  fill(232, 190, 172);//nose color 
-  arc(198, 205, 28, 18, 270, 90);
-
-  noStroke();
-  fill(92, 64, 51); //bangs color
-  rect(200, 95, 99, 43, 58); //bangs
+  //Table
+  fill(210,180,140); 
+  rect(0.5, 220, 600, 300, 80);
 
   
-  fill(92, 64, 51); //brow color 
-  rect(160, 140, 40, 5); //left brow 
-  rect(238, 140, 40, 5); //right brow
+  //Plate Shadow
+  fill(0,0,0, 190);
+  ellipse(175, 260, 190, 100);
 
+  //Plate
+  fill(255, 255, 255);
+  ellipse(190, 250, 200, 100);
 
+  //Left Pear Shadow
+  fill(0,0,0, 250);
+  ellipse(90, 198, 65, 80);
+
+  //Left Pear Stem
+  fill(0);
+  rect(70, 230, 3, 10); 
+
+  //Left Pear
+  fill(0,100,0);
+  ellipse(85, 190, 70, 90);
+
+  //Apple 1 Shadow
+  fill(0);
+  ellipse(160, 228, 69, 65);
+
+  //Apple 1 Left
+  fill(255,215,0);
+  ellipse(150, 230, 70, 70);
   
+  //Apple 2 Shadow Right
+  fill(0);
+  ellipse(208, 244, 60, 70);
+
+  //Pear Behind Right Apple
+  fill(0,100,0);
+  ellipse(221, 234, 60, 70);
+
+  //Apple 2 Right
+  fill(255,215,0);
+  ellipse(215, 240, 70, 70);
+
+  //Apple 2 Stem
+  fill(0);
+  ellipse(210, 240, 5, 5 );
+
+  //Apple 3 Shadow
+  fill(0);
+  ellipse(195, 180, 60, 65);
+
+  //Apple 3
+  fill(255,215,0);
+  ellipse(200, 175, 70, 70);
+
+  //Apple 3 Stem
+  fill(0);
+  ellipse(200, 180, 5, 5 );
 }
+
 
