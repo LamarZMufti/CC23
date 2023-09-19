90486bde859657a7831a10fa5da41721dd0f2882
LamarZMufti <144357682+LamarZMufti@users.noreply.github.com>
2023-09-19 17:14:06 +0300

    Bouncing Ball

--- sketch.js
+++ sketch.js
@@ -1,67 +1,45 @@
+let x, y; // ball x and x position
+let xDir, yDir; // direction of the ball
+let size; // size of the ball
+
 function setup() {
-  createCanvas(400, 400);
-  background(135,206,250);
-  
-  //Table
-  fill(210,180,140); 
-  rect(0.5, 220, 600, 300, 80);
+  createCanvas(windowWidth, windowHeight);
 
-  
-  //Plate Shadow
-  fill(0,0,0, 190);
-  ellipse(175, 260, 190, 100);
+  // random directions
+  xDir = random(-20, 10);
+  yDir = random(-10, 10);
 
-  //Plate
-  fill(255, 255, 255);
-  ellipse(190, 250, 200, 100);
+  // random size
+  size = random(10, 10);
 
-  //Left Pear Shadow
-  fill(0,0,0, 250);
-  ellipse(90, 198, 65, 80);
-
-  //Left Pear Stem
-  fill(0);
-  rect(70, 230, 3, 10); 
-
-  //Left Pear
-  fill(0,100,0);
-  ellipse(85, 190, 70, 90);
-
-  //Apple 1 Shadow
-  fill(0);
-  ellipse(160, 228, 69, 65);
-
-  //Apple 1 Left
-  fill(255,215,0);
-  ellipse(150, 230, 70, 70);
-  
-  //Apple 2 Shadow Right
-  fill(0);
-  ellipse(208, 244, 60, 70);
-
-  //Pear Behind Right Apple
-  fill(0,100,0);
-  ellipse(221, 234, 60, 70);
-
-  //Apple 2 Right
-  fill(255,215,0);
-  ellipse(215, 240, 70, 70);
-
-  //Apple 2 Stem
-  fill(0);
-  ellipse(210, 240, 5, 5 );
-
-  //Apple 3 Shadow
-  fill(0);
-  ellipse(195, 180, 60, 65);
-
-  //Apple 3
-  fill(255,215,0);
-  ellipse(200, 175, 70, 70);
-
-  //Apple 3 Stem
-  fill(0);
-  ellipse(200, 180, 5, 5 );
+  // starting point is somewhere in the 
+  // canvas, not touching a border
+  x = random(size, width - size);
+  y = random(size, height - size);
+  noStroke();
+  fill(255,140,0);
 }
 
+function draw() {
+  // a little motion blur
+  background(70, 20);
+  // draw our ball
+  ellipse(x, y, size);
 
+  // if the ball touches the left or right side
+  if (x >= width - size / 2 || x <= size / 2) {
+    // reverse it's direction
+    xDir = xDir * -1;
+    // the follwing line is the lazy way to reverse dir
+    // xDir *= -1;
+  }
+
+  // if the ball touches the ceilign or floor
+  if (y >= height - size / 2 || y <= size / 2) {
+    yDir = yDir * -1;
+  }
+
+  // update the position of the ball for the next loop
+  x = x + xDir;
+  y = y + yDir;
+}
