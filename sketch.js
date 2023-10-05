dbec59c93508b7ee0518304e19e8bb0513508dd5
LamarZMufti <144357682+LamarZMufti@users.noreply.github.com>
2023-10-05 04:09:06 +0300

    Time-based Media

--- sketch.js
+++ sketch.js
@@ -1,30 +1,41 @@
-let brushSize = 50;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
+  angleMode(DEGREES);
 }
 
 function draw() {
+  background(0);
   
-}
+  let end = map(mouseX, 0, width, 0, 360); //mouse control over full arc 
 
-function mouseDragged(){
+  strokeWeight(10); //first arc
+  stroke(0);
+  arc(700, 400, 400, 400, 0, end);
+
+  strokeWeight(10); //second arc
+  stroke(0);
+  arc(700, 400, 300, 300, 0, end);
   
-  if(mouseX < width/2){
-    
-    fill(random(5), random(255), random(255));
-    
-  } else {
-    
-    fill(random(255));
-  }
-  
-  translate(500, 500);
-  noStroke();
-  for (let i = 0; i < 20; i ++) {
-    ellipse(mouseX, mouseY, brushSize);
-    rotate(PI/5);
-  }
+  strokeWeight(10); //third arc
+  stroke(0);
+  arc(700, 400, 250, 250, 0, end);
+
+  strokeWeight(10); //fourth arc
+  stroke(0);
+  arc(700, 400, 200, 200, 0, end);
+
+  strokeWeight(10); //fifth arc
+  stroke(0);
+  arc(700, 400, 150, 150, 0, end);
+
+  strokeWeight(10); //sixth arc
+  stroke(0);
+  arc(700, 400, 100, 100, 0, end);
+
+  strokeWeight(10); //seventh arc
+  stroke(0);
+  arc(700, 400, 50, 50, 0, end);
+
+
   
 }
