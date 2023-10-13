4ef583738d6067ae8415b848cf93a29f18695bbb
LamarZMufti <144357682+LamarZMufti@users.noreply.github.com>
2023-10-13 06:20:32 +0300

    Button

--- sketch.js
+++ sketch.js
@@ -1,41 +1,70 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  angleMode(DEGREES);
-}
+let angle = 0.0;
+let x = 100;
+let y = 100;
+let w = 60;
+let h = 60;
+
+let speedx = 0.5;
+let speedy = 0.5;
+
+let input;
+let button;
+let wordGenerator;
 
 function draw() {
-  background(0);
+  let sinval = sin(angle);
+  let sky1 = map(sinval, -1, 1, 0, 255);
+  let sky2 = map(sinval, -1, 1, 255, 0);
+  background(sky1);
+  angle += 0.1;
   
-  let end = map(mouseX, 0, width, 0, 360); //mouse control over full arc 
-
-  strokeWeight(10); //first arc
-  stroke(0);
-  arc(700, 400, 400, 400, 0, end);
-
-  strokeWeight(10); //second arc
-  stroke(0);
-  arc(700, 400, 300, 300, 0, end);
+  fill(sky2);
+  noStroke();
+  ellipse(random()*width,random()*height,3,3);
+  ellipse(random()*width,random()*height,5,5);
+  ellipse(random()*width,random()*height,1,1);
+  ellipse(random()*width,random()*height,2,2);
+  ellipse(random()*width,random()*height,5,5);
   
-  strokeWeight(10); //third arc
-  stroke(0);
-  arc(700, 400, 250, 250, 0, end);
+  
+}
+ 
 
-  strokeWeight(10); //fourth arc
-  stroke(0);
-  arc(700, 400, 200, 200, 0, end);
-
-  strokeWeight(10); //fifth arc
-  stroke(0);
-  arc(700, 400, 150, 150, 0, end);
-
-  strokeWeight(10); //sixth arc
-  stroke(0);
-  arc(700, 400, 100, 100, 0, end);
-
-  strokeWeight(10); //seventh arc
-  stroke(0);
-  arc(700, 400, 50, 50, 0, end);
+function setup() {
+  // create canvas
+  createCanvas(windowWidth, windowHeight);
+  background(255);
 
 
+  input = createInput();
+  input.position(650, 400);
+
+  button = createButton('press');
+  button.position(800, 400);
+  button.mousePressed(display);
+
+
+  wordGenerator = createElement('h1', 'Type Anything');
+  wordGenerator.position(650, 330);
+  
+  
+ 
+}
+
+function display() { 
+  const word = input.value();
+  wordGenerator.html(word);
+  input.value('');
+
+  for (let i = 0; i < 1000; i++) {
+    push();
+    fill(random(0), 0, 0);
+    translate(random(width), random(height));
+    rotate(random(2 * PI));
+    text(word, 0, 0);
+    pop();
+  }
+
+  
   
 }
