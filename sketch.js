e7af30dcad002cd12fdb1215b06f655b478999d7
LamarZMufti <144357682+LamarZMufti@users.noreply.github.com>
2023-10-17 08:20:36 +0300

    Meme

--- sketch.js
+++ sketch.js
@@ -1,58 +1,53 @@
+let images = []; // Array to store loaded image
+let img; // Current image variable
+let xPos, yPos; // Position variables
+let xSpeed, ySpeed; // Speed variables
+let rot = 0.0; // Rotation angle
+let sc = 1.0; // Scaling factor
+
+function preload() {
+  // Load image from the internet
+  let imageURLs = ["https://hips.hearstapps.com/hmg-prod/images/ai-flaw-memes-1677257023.jpg?crop=0.520xw:0.988xh;0.225xw,0&resize=1200:*"]; // image URL
+  
+  for (let i = 0; i < imageURLs.length; i++) {
+    images[i] = loadImage(imageURLs[i]);
+  }
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  angleMode(DEGREES);
-  frameRate(1); // Update every second
+  createCanvas(windowWidth, windowHeight); 
+
+  // Choose a random image from the loaded images
+  img = random(images);
+
+  // Set initial positions and speeds
+  xPos = random(width);
+  yPos = random(height);
+  xSpeed = random(-3, 3);
+  ySpeed = random(-3, 3);
 }
 
 function draw() {
-  background(255);
-  translate(width / 2, height / 2);
-  rotate(-90); // Adjust for 0 degrees at the top
+  background(0, 5); // Background animates with low opacity
 
-  let now = new Date();
-  let seconds = now.getSeconds();
-  let minutes = now.getMinutes();
-  let hours = now.getHours() % 12; // Convert 24-hour time to 12-hour time
+  imageMode(CENTER);
+  push();
+  translate(xPos, yPos); // Translate image's position
+  rotate(rot); // Rotate the image
+  scale(4.0 * noise(sc)); // Scale the image with noise
+  image(img, 0, 0, img.width / 2, img.height / 2); // Display the image
+  pop();
 
-  // Draw clock face
-  stroke(0);
-  strokeWeight(8);
-  fill(0);
-  ellipse(0, 0, 300, 300);
+  rot += 0.01; // Increment rotation angle
+  xPos += xSpeed; // Update horizontal position
+  yPos += ySpeed; // Update vertical position
 
-  // Draw hour hand
-  stroke(255);
-  strokeWeight(8);
-  line(0, 0, 60 * cos(360 * hours / 12), 60 * sin(360 * hours / 12));
-
-  // Draw minute hand
-  stroke(255);
-  strokeWeight(5);
-  line(0, 0, 90 * cos(360 * minutes / 60), 90 * sin(360 * minutes / 60));
-
-  // Draw second hand
-  stroke(150, 50, 100); // purplish
-  strokeWeight(1);
-  line(0, 0, 100 * cos(360 * seconds / 60), 100 * sin(360 * seconds / 60));
-
-  stroke(255, 150);
-  secondComponent(seconds);
-}
-
-function secondComponent(seconds) {
-  for (let i = 0; i < 60; i++) {
-    let x = 135 * cos(360 * i / 60);
-    let y = 135 * sin(360 * i / 60);
-
-   
-    if (i % 5 === 0) {
-      noFill();
-      ellipse(x, y, 15, 15); // Draw circles at hour positions
-    } else {
-      if (i <= seconds) {
-        noFill();
-        line(x - 10, y - 2, 10, 4); // Draw lines as seconds pass
-      }
-    }
+  if (xPos >= width || xPos <= 0) {
+    xSpeed *= -1; // Bounce if hitting horizontal boundaries
   }
+  if (yPos >= height || yPos <= 0) {
+    ySpeed *= -1; // Bounce if hitting vertical boundaries
+  }
+
+  sc += 0.001; // Increment scaling factor
 }
