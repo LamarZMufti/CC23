7aa655a66699cd8eb96f7ea635586958111fce25
LamarZMufti <144357682+LamarZMufti@users.noreply.github.com>
2023-10-26 06:52:26 +0300

    Data Sources

--- sketch.js
+++ sketch.js
@@ -1,53 +1,44 @@
-let images = []; // Array to store loaded image
-let img; // Current image variable
-let xPos, yPos; // Position variables
-let xSpeed, ySpeed; // Speed variables
-let rot = 0.0; // Rotation angle
-let sc = 1.0; // Scaling factor
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
-function preload() {
-  // Load image from the internet
-  let imageURLs = ["https://hips.hearstapps.com/hmg-prod/images/ai-flaw-memes-1677257023.jpg?crop=0.520xw:0.988xh;0.225xw,0&resize=1200:*"]; // image URL
-  
-  for (let i = 0; i < imageURLs.length; i++) {
-    images[i] = loadImage(imageURLs[i]);
-  }
+// Event listener
+searchBtn.addEventListener('click', getMealList);
+
+// Get meal list that matches with ingredients
+function getMealList() {
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=${searchInputTxt}&number=5&ignorePantry=true&ranking=1`, {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+            'X-RapidAPI-Key': '9712c3d31520993d5755978d79aceb64170727d6b61187a53ca74d2c720ccd1be1ee7a23cfa40f275443ac807b29a835'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if (data.length > 0) {
+            data.forEach(meal => {
+                html += `
+                <div class="meal-item" data-id="${meal.id}">
+                    <div class="meal-img">
+                        <img src="${meal.image}" alt="food">
+                    </div>
+                    <div class="meal-name">
+                        <h3>${meal.title}</h3>
+                        <a href="#" class="recipe-btn">Get Recipe</a>
+                    </div>
+                </div>`;
+            });
+        } else {
+            html = '<p>No recipes found.</p>';
+        }
+        mealList.innerHTML = html;
+    })
+    .catch(error => {
+        console.error(error);
+    });
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight); 
-
-  // Choose a random image from the loaded images
-  img = random(images);
-
-  // Set initial positions and speeds
-  xPos = random(width);
-  yPos = random(height);
-  xSpeed = random(-3, 3);
-  ySpeed = random(-3, 3);
-}
-
-function draw() {
-  background(0, 5); // Background animates with low opacity
-
-  imageMode(CENTER);
-  push();
-  translate(xPos, yPos); // Translate image's position
-  rotate(rot); // Rotate the image
-  scale(4.0 * noise(sc)); // Scale the image with noise
-  image(img, 0, 0, img.width / 2, img.height / 2); // Display the image
-  pop();
-
-  rot += 0.01; // Increment rotation angle
-  xPos += xSpeed; // Update horizontal position
-  yPos += ySpeed; // Update vertical position
-
-  if (xPos >= width || xPos <= 0) {
-    xSpeed *= -1; // Bounce if hitting horizontal boundaries
-  }
-  if (yPos >= height || yPos <= 0) {
-    ySpeed *= -1; // Bounce if hitting vertical boundaries
-  }
-
-  sc += 0.001; // Increment scaling factor
-}
