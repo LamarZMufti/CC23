46ab9f40f57fefc2368747c74b2838dcd4e1ba1c
LamarZMufti <144357682+LamarZMufti@users.noreply.github.com>
2023-11-06 20:10:05 -0500

    Teachable Machine

--- sketch.js
+++ sketch.js
@@ -1,32 +1,58 @@
-let apiKey = '6c2dec0ef4cfdedee9cfbc13a532ecf7';
-let weatherData;
+let classifier;
+// Model URL
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/bXy2kDNi/';
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+
+// Load the model first
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  let searchBtn = select('#search-btn');
-  searchBtn.mousePressed(getWeather);
-  
+  createCanvas(320, 260);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  video.hide();
+
+  flippedVideo = ml5.flipImage(video)
+  // Start classifying
+  classifyVideo();
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  textSize(20);
+  background(0);
+  // Draw the video
+  image(flippedVideo, 0, 0);
+
+  // Draw the label
+  fill(255);
+  textSize(16);
   textAlign(CENTER);
-  if (weatherData) {
-    text(`City: ${weatherData.name}`, width / 2, 30);
-    text(`Temperature: ${weatherData.main.temp} °C`, width / 2, 60);
-    text(`Description: ${weatherData.weather[0].description}`, width / 2, 90);
+  text(label, width / 2, height - 4);
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video)
+  classifier.classify(flippedVideo, gotResult);
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
   }
-}
-
-function getWeather() {
-  let city = select('#search-input').value();
-  let units = 'metric'; // 
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
-  loadJSON(apiUrl, gotWeatherData);
-}
-
-function gotWeatherData(data) {
-  weatherData = data;
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+  // Classifiy again!
+  classifyVideo();
 }
